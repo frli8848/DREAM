3399530c033e887c1e0af33cd62778fd11391c9c
Fredrik Lingvall <fredrik.lingvall@protonmail.com>
2022-03-23 13:14:27 +0100

    Made the cmake FindFFTW module find static FFTW libs too

--- cmake/Modules/FindFFTW.cmake
+++ cmake/Modules/FindFFTW.cmake
@@ -51,7 +51,48 @@ else (FFTW_FOUND)
   endif (FFTW_FIND_REQUIRED)
 endif (FFTW_FOUND)
 
+#
+# Static libs
+#
+
+find_library (FFTW_STATIC_LIBRARIES NAMES libfftw3-3.a libfftw3.a fftw3.a PATHS lib)
+if (NOT FFTW_STATIC_LIBRARIES)
+  message (STATUS "Could not find the double precision static FFTW library")
+endif (NOT FFTW_STATIC_LIBRARIES)
+
+find_library (SFFTW_STATIC_LIBRARIES NAMES libfftw3f-3.a libfftw3f.a fftw3f.a PATHS lib)
+if (NOT SFFTW_STATIC_LIBRARIES)
+  message (STATUS "Could not find the single precision static FFTW library")
+endif (NOT SFFTW_STATIC_LIBRARIES)
+
+if (FFTW_INCLUDE_DIR AND FFTW_STATIC_LIBRARIES AND SFFTW_STATIC_LIBRARIES)
+  set (FFTW_STATIC_FOUND TRUE)
+endif (FFTW_INCLUDE_DIR AND FFTW_STATIC_LIBRARIES AND SFFTW_STATIC_LIBRARIES)
+
+if (FFTW_STATIC_FOUND)
+
+  if (NOT FFTW_FIND_QUIETLY)
+    message (STATUS "Found fftw3.h: ${FFTW_INCLUDE_DIR}")
+  endif (NOT FFTW_FIND_QUIETLY)
+
+  if (NOT FFTW_FIND_QUIETLY)
+    message (STATUS "Found static FFTW: ${FFTW_STATIC_LIBRARIES}")
+  endif (NOT FFTW_FIND_QUIETLY)
+
+  if (NOT FFTW_FIND_QUIETLY)
+    message (STATUS "Found static FFTW: ${SFFTW_STATIC_LIBRARIES}")
+  endif (NOT FFTW_FIND_QUIETLY)
+
+else (FFTW_STATIC_FOUND)
+  if (FFTW_FIND_REQUIRED)
+    message (FATAL_ERROR "Could not find static FFTW")
+  endif (FFTW_FIND_REQUIRED)
+endif (FFTW_STATIC_FOUND)
+
+
+
 mark_as_advanced (FFTW_INCLUDE_DIR
   FFTW_LIBRARIES
+  FFTW_STATIC_LIBRARIES
   SFFTW_LIBRARIES
   FFTW_FOUND)

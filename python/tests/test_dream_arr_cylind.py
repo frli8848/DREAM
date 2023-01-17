5bd8de23b314771257b76132a01927fd05c32911
Fredrik Lingvall <fredrik.lingvall@protonmail.com>
2023-01-17 16:47:26 +0100

    Inital Python support for dream_arr_cylind

---
+++ python/tests/test_dream_arr_cylind.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+#import sys
+#sys.path.insert(0, '../')
+
+import dream_arr_cylind as dar
+
+#plt.rcParams['text.usetex'] = True
+
+#setup_dream_parameters
+Fs = 4.0;                       # Sampling freq. [MHz].
+Ts = 1/Fs;                      # [us].
+
+z = 10.0;
+
+# One point.
+xo = 0;
+yo = 0;
+zo = z;
+ro = np.asmatrix([xo,yo,zo]);
+Ro = ro;
+print("Observation point (x,y,z) = (%f, %f, %f)" % (xo,yo,zo))
+
+# Descretization parameters.
+dx = 0.05;                # [mm].
+dy = 0.05;                # [mm]
+dt = Ts;                  # [us].
+nt = 300.0;                 # Length of spatial impulse response vector.
+s_par = np.asmatrix([dx,dy,dt,nt]);
+
+t = np.linspace(0.0, Ts*float(nt-1), num=int(nt), endpoint=True)
+
+# Material parameters.
+v     = 1.0;                    # Normal velocity.
+cp    = 1000.0;                 # Sound speed.
+alpha = 0.0;                    # Absorbtion (dB/cm Hz).
+m_par = np.asmatrix([v,cp,alpha]);
+
+#t_z = z*1.0e-3/cp * 1.0e6;      # us
+delay = np.asmatrix(0.0);
+
+# Element size [mm].
+a = 0.8;
+b = 20.0;
+Rcurv = 10.0;
+geom_par = np.asmatrix([a,b,Rcurv]);
+
+# Grid function (position vectors of the elements).
+gx = np.linspace(-10.0, 10.0, num=21, endpoint=True)
+gx = gx.reshape(21,1)
+[num_elements,n] = np.shape(gx)
+gy = np.zeros((num_elements,1), dtype=float);
+gz = np.zeros((num_elements,1), dtype=float);
+G = gx
+G = np.hstack((G, gy))
+G = np.hstack((G, gz))
+
+# Focusing parameters.
+#foc_met = "off";
+foc_met = "x";
+#foc_met = "y";
+#foc_met = "xy";
+#foc_met = "x+y";
+focal = np.asmatrix(10.0);      # Focus depth
+
+## Beam steering.
+steer_met = "off";
+#steer_met = "x";
+#steer_met = "y";
+#steer_met = "xy";
+
+theta  = 0.0;                   # Angle in x-direction.
+phi    = 0.0;                   # Angle in y-direction.
+steer_par = np.asmatrix([theta,phi]);
+
+## Apodization.
+apod_met = "off";
+#apod_met = "ud";                       # User defined.
+#apod_met = "triangle";
+#apod_met = "gauss";
+#apod_met = "raised";                   # Raised cosine.
+#apod_met = "simply";                   # Simply supported.
+#apod_met = "clamped";
+apod = np.ones((num_elements,1));     # Apodization weights for "ud".
+win_par = 1.0; # Parameter for raised cos and Gaussian apodization functions.
+
+H = dar.dream_arr_cylind(Ro,geom_par,G,s_par,delay,m_par,foc_met,focal,steer_met,steer_par,apod_met,apod,win_par,"stop")
+
+#fig1 = plt.figure(1)
+fig, axs = plt.subplots(2)
+#plt.clf()
+
+axs[0].plot(t, H, 'b-', label='Attenuation alpha = 0 [dB/cm MHz]', linewidth=2, markersize=12)
+
+alpha  = 5.0;                   # Absorbtion (dB/cm Hz).
+m_par = np.asmatrix([v,cp,alpha]);
+Hatt = dar.dream_arr_cylind(Ro,geom_par,G,s_par,delay,m_par,foc_met,focal,steer_met,steer_par,apod_met,apod,win_par,"stop");
+axs[0].plot(t, Hatt, 'r--',label='Attenuation alpha = 5 [dB/cm MHz]', linewidth=2, markersize=12)
+
+axs[0].set_title('Array with focused cylindrical transducer at Rcurv=10 [mm]')
+axs[0].legend()
+
+H = dar.dream_arr_cylind(Ro,geom_par,G,s_par,delay,m_par,foc_met,focal,steer_met,steer_par,apod_met,apod,win_par,"stop")
+
+#fig2 = plt.figure(2)
+#plt.clf()
+
+Rcurv = -10.0;
+geom_par = np.asmatrix([a,b,Rcurv]);
+
+alpha  = 0.0;                   # Absorbtion (dB/cm Hz).
+m_par = np.asmatrix([v,cp,alpha]);
+
+H = dar.dream_arr_cylind(Ro,geom_par,G,s_par,delay,m_par,foc_met,focal,steer_met,steer_par,apod_met,apod,win_par,"stop")
+axs[1].plot(t, H, 'b-', label='Attenuation alpha = 0 [dB/cm MHz]', linewidth=2, markersize=12)
+
+alpha  = 5.0;                   # Absorbtion (dB/cm Hz).
+m_par = np.asmatrix([v,cp,alpha]);
+Hatt = dar.dream_arr_cylind(Ro,geom_par,G,s_par,delay,m_par,foc_met,focal,steer_met,steer_par,apod_met,apod,win_par,"stop");
+axs[1].plot(t, Hatt, 'r--',label='Attenuation alpha = 5 [dB/cm MHz]', linewidth=2, markersize=12)
+
+axs[1].set_title('Array with defocused cylindrical transducer at Rcurv=-10 [mm]')
+axs[1].legend()
+
+plt.show()

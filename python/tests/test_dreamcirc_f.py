49bdeb6874c842ed3e4d3c4ff200f33214dd7201
Fredrik Lingvall <fredrik.lingvall@protonmail.com>
2023-01-17 21:46:59 +0100

    Inital Python support for dreamcirc_f

---
+++ python/tests/test_dreamcirc_f.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+#import sys
+#sys.path.insert(0, '../')
+
+import dreamcirc_f as dr
+
+#plt.rcParams['text.usetex'] = True
+
+#setup_dream_parameters
+Fs = 4.0;                       # Sampling freq. [MHz].
+Ts = 1/Fs;                      # [us].
+
+z = 10.0;
+
+# One point.
+xo = 0;
+yo = 0;
+zo = z;
+ro = np.asmatrix([xo,yo,zo]);
+Ro = ro;
+print("Observation point (x,y,z) = (%f, %f, %f)" % (xo,yo,zo))
+
+# Descretization parameters.
+dx = 0.05;                # [mm].
+dy = 0.05;                # [mm]
+dt = Ts;                  # [us].
+nt = 300.0;               # Length of spatial impulse response vector.
+s_par = np.asmatrix([dx,dy,dt,nt]);
+
+t = np.linspace(0.0, Ts*float(nt-1), num=int(nt), endpoint=True)
+
+# Material parameters.
+v     = 1.0;                    # Normal velocity.
+cp    = 1000.0;                 # Sound speed.
+alpha = 0.0;                    # Absorbtion [dB/cm Hz].
+m_par = np.asmatrix([v,cp,alpha]);
+
+#t_z = z*1.0e-3/cp * 1.0e6;      # us
+delay = np.asmatrix(0.0);
+
+# Element size [mm].
+R = 10.0;
+geom_par = np.asmatrix([R]);
+
+# Focusing parameters.
+#foc_met = "off";
+#foc_met = "x";
+#foc_met = "y";
+foc_met = "xy";
+#foc_met = "x+y";
+focal = np.asmatrix(10.0);
+
+H = dr.dreamcirc_f(Ro,geom_par,s_par,delay,m_par,foc_met,focal,"stop")
+
+fig = plt.figure(1)
+plt.clf()
+
+plt.plot(t, H, 'b-', label='Attenuation alpha = 0 [dB/cm MHz]', linewidth=2, markersize=12)
+
+alpha  = 5.0;                   # Absorbtion (dB/cm Hz).
+m_par = np.asmatrix([v,cp,alpha]);
+Hatt = dr.dreamcirc_f(Ro,geom_par,s_par,delay,m_par,foc_met,focal,"stop");
+plt.plot(t, Hatt, 'r--',label='Attenuation alpha = 5 [dB/cm MHz]', linewidth=2, markersize=12)
+
+plt.title('Focused circular transducer')
+
+plt.xlabel('t [us]')
+plt.ylabel('h_{SIR} [m/s]')
+
+plt.legend()
+plt.show()

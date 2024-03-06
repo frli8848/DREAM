70914a92720d35ae6a0510793cf9e9c38586bfb3
Fredrik Lingvall <fredrik.lingvall@protonmail.com>
2024-03-06 20:59:15 +0100

    Initial Julia support for threaded 1D time-domain convolution #1013
    This adds support for the standard non-inplace modes of threaded
    1D convolution foer Julia (and some cleanup of the Octave bindings too).

--- julia/src/jl_conv_p.cc
+++ julia/src/jl_conv_p.cc
@@ -1,6 +1,6 @@
 /***
 *
-* copyright (C) 2006,2007,2008,2009,2014,2015,2016,2022,2023 Fredrik Lingvall
+* Copyright (C) 2024 Fredrik Lingvall
 *
 * This file is part of the DREAM Toolbox.
 *
@@ -24,11 +24,11 @@
 #include <csignal>
 #include <thread>
 
-#include <octave/oct.h>
-
 #include "dream.h"
 #include "affinity.h"
 #include "conv.h"
+
+#include "arg_parser_julia.h"
 
 /***
  *
@@ -48,14 +48,14 @@ volatile int running;
 
 typedef struct
 {
-  octave_idx_type col_start;
-  octave_idx_type col_stop;
+  dream_idx_type col_start;
+  dream_idx_type col_stop;
   double *A;
-  octave_idx_type A_M;
-  octave_idx_type A_N;
+  dream_idx_type A_M;
+  dream_idx_type A_N;
   double *B;
-  octave_idx_type B_M;
-  octave_idx_type B_N;
+  dream_idx_type B_M;
+  dream_idx_type B_N;
   double *Y;
   ConvMode conv_mode;
 } DATA;
@@ -80,9 +80,9 @@ void sig_keyint_handler(int signum);
 void* smp_dream_conv(void *arg)
 {
   DATA D = *(DATA *)arg;
-  octave_idx_type    col_start=D.col_start, col_stop=D.col_stop;
+  dream_idx_type    col_start=D.col_start, col_stop=D.col_stop;
   double *A = D.A, *B = D.B, *Y = D.Y;
-  octave_idx_type A_M = D.A_M, B_M = D.B_M, B_N = D.B_N;
+  dream_idx_type A_M = D.A_M, B_M = D.B_M, B_N = D.B_N;
   ConvMode conv_mode = D.conv_mode;
 
   // Do the convolution.
@@ -95,7 +95,7 @@ void* smp_dream_conv(void *arg)
       conv( &A[0+n*A_M], A_M, B, B_M, &Y[0+n*(A_M+B_M-1)], conv_mode);
 
     if (running == false) {
-      octave_stdout << "conv_p: thread for column " << col_start+1 << " -> " << col_stop << "bailing out!\n";
+      std::cout << "conv_p: thread for column " << col_start+1 << " -> " << col_stop << "bailing out!\n";
       break;
       //return(NULL);
     }
@@ -123,129 +123,31 @@ void sig_keyint_handler(int signum) {
   //printf("Caught signal SIGINT.\n");
 }
 
-/***
- *
- * Octave (oct) gateway function for CONV_P.
- *
- ***/
 
-DEFUN_DLD (conv_p, args, nlhs,
-           "-*- texinfo -*-\n\
-@deftypefn {Loadable Function} {}  [Y] = conv_p(A, B).\n\
-\n\
-CONV_P Computes one dimensional convolutions of the columns in the matrix A and the matrix (or vector) B.\n\
-\n\
-Input parameters:\n\
-\n\
-@table @samp\n\
-@item A\n\
-An MxN matrix.\n\
-@item B\n\
-A KxN matrix or a K-length vector. If B is a vector each column in A is convolved with the vector B.\n\
-\n\
-Output parameter:\n\
-\n\
-@table @samp\n\
-@item Y\n\
-The (M+K-1)xN output matrix.\n\
-@end table\n\
-\n\
-conv_p is an oct-function that is a part of the DREAM Toolbox available at\n\
- @url{https://github.com/frli8848/DREAM}.\n\
-\n\
-Copyright @copyright{} 2006-2023 Fredrik Lingvall.\n\
-@seealso {fftconv_p, fftconv, conv}\n\
-@end deftypefn")
+namespace jl = jlcxx;
+
+jl::ArrayRef<double, 2> jl_conv_p(jl::ArrayRef<double, 2> jl_A,
+                                  jl::ArrayRef<double, 2> jl_B);
+
+jl::ArrayRef<double, 2> jl_conv_p(jl::ArrayRef<double, 2> jl_A,
+                                  jl::ArrayRef<double, 2> jl_B)
 {
-  double *A = nullptr, *B = nullptr, *Y = nullptr;
+  ArgParser ap;
   sighandler_t old_handler, old_handler_abrt, old_handler_keyint;
-  octave_idx_type col_start, col_stop, A_M, A_N, B_M, B_N;
-  bool is_set = false;
   DATA *D = nullptr;
   ConvMode conv_mode = ConvMode::equ;
 
-  octave_value_list oct_retval;
+  double *A = static_cast<double*>(ap.get_data(jl_A));
+  dream_idx_type A_M = (dream_idx_type) ap.get_m(jl_A);
+  dream_idx_type A_N = (dream_idx_type) ap.get_n(jl_A);
 
-  int nrhs = args.length ();
-
-  // Check for proper inputs arguments.
-
-  switch (nrhs) {
-
-  case 0:
-  case 1:
-    dream_err_msg("conv_p requires 2 to 4 input arguments!");
-    return oct_retval;
-    break;
-
-  case 2:
-    if (nlhs > 1) {
-      dream_err_msg("Too many output arguments for conv_p!");
-      return oct_retval;
-    }
-    break;
-
-  case 3:
-    if (nlhs > 0) {
-      dream_err_msg("No output arguments required for conv_p in in-place operating mode!");
-      return oct_retval;
-    }
-    break;
-
-  case 4:
-    if ( args(3).is_string() ) { // 4:th arg is a string '=', '+=', or '-='.
-
-      std::string ip_mode = args(3).string_value();
-
-      // Valid strings are:
-      //  '='  : In-place replace mode.
-      //  '+=' : In-place add mode.
-      //  '-=' : In-place sub mode.
-
-      is_set = false;
-
-      if (ip_mode.compare("=") == 0) {
-        conv_mode=ConvMode::equ;
-        is_set = true;
-      }
-
-      if (ip_mode.compare("+=") == 0) {
-        conv_mode=ConvMode::sum;
-        is_set = true;
-      }
-
-      if (ip_mode.compare("-=") == 0) {
-        conv_mode=ConvMode::neg;
-        is_set = true;
-      }
-
-      if (is_set == false) {
-        dream_err_msg("Non-valid string in arg 4!");
-        return oct_retval;
-      }
-    }
-    break;
-
-  default:
-    dream_err_msg("conv_p requires 2 to 4 input arguments!");
-    return oct_retval;
-    break;
-  }
-
-  const Matrix tmp0 = args(0).matrix_value();
-  A_M = tmp0.rows();
-  A_N = tmp0.cols();
-  A = (double*) tmp0.data();
-
-  const Matrix tmp1 = args(1).matrix_value();
-  B_M = tmp1.rows();
-  B_N = tmp1.cols();
-  B = (double*) tmp1.data();
+  double *B = static_cast<double*>(ap.get_data(jl_B));
+  dream_idx_type B_M = (dream_idx_type) ap.get_m(jl_B);
+  dream_idx_type B_N = (dream_idx_type) ap.get_n(jl_B);
 
   // Check that arg 2.
   if ( B_M != 1 && B_N !=1 && B_N != A_N) {
-    dream_err_msg("Argument 2 must be a vector or a matrix with the same number of rows as arg 1!");
-    return oct_retval;
+    throw std::runtime_error("Argument 2 must be a vector or a matrix with the same number of rows as arg 1!");
   }
 
   if (  B_M == 1 || B_N == 1 ) { // B is a vector.
@@ -258,7 +160,7 @@ Copyright @copyright{} 2006-2023 Fredrik Lingvall.\n\
   //
 
   // Get number of CPU cores (including hypethreading, C++11)
-  octave_idx_type nthreads = std::thread::hardware_concurrency();
+  dream_idx_type nthreads = std::thread::hardware_concurrency();
 
   // Read DREAM_NUM_THREADS env var
   if(const char* env_p = std::getenv("DREAM_NUM_THREADS")) {
@@ -289,37 +191,13 @@ Copyright @copyright{} 2006-2023 Fredrik Lingvall.\n\
     std::cerr << "Couldn't register SIGINT signal handler!" << std::endl;
   }
 
-  if (nrhs == 2) { // Test for in-place/normal mode.
 
-    //
-    // Normal (non in-place) mode.
-    //
+  // Create an output matrix for the impulse responses
+  jl_value_t *matrix_type = jl_apply_array_type((jl_value_t*) jl_float64_type,2);
+  jl_array_t *jl_Y_array = jl_alloc_array_2d(matrix_type, A_M+B_M-1, A_N);
+  double *Y = (double *) jl_array_data(jl_Y_array);
+  auto jl_Y_mat = jl::ArrayRef<double, 2>(jl_Y_array);
 
-    Matrix Ymat(A_M+B_M-1, A_N);
-    Y = (double*) Ymat.data();
-
-    oct_retval.append(Ymat);
-
-  } else {
-
-    //
-    // In-place mode.
-    //
-
-    if (args(2).matrix_value().rows() != A_M+B_M-1) {
-      dream_err_msg("Wrong number of rows in argument 3!");
-      return oct_retval;
-    }
-
-    if (args(2).matrix_value().cols() != A_N) {
-      dream_err_msg("Wrong number of columns in argument 3!");
-      return oct_retval;
-    }
-
-    const Matrix Ytmp = args(2).matrix_value();
-
-    Y = (double*) Ytmp.data();
-  }
 
   //
   // Call the CONV subroutine.
@@ -330,21 +208,19 @@ Copyright @copyright{} 2006-2023 Fredrik Lingvall.\n\
   // Allocate local data.
   D = (DATA*) malloc(nthreads*sizeof(DATA));
   if (!D) {
-    dream_err_msg("Failed to allocate memory for thread data!");
-    return oct_retval;
+    throw std::runtime_error("Failed to allocate memory for thread data!");
   }
 
   // Allocate mem for the threads.
   std::thread *threads = new std::thread[nthreads]; // Init thread data.
   if (!threads) {
-    dream_err_msg("Failed to allocate memory for threads!");
-    return oct_retval;
+    throw std::runtime_error("Failed to allocate memory for threads!");
   }
 
-  for (octave_idx_type thread_n = 0; thread_n < nthreads; thread_n++) {
+  for (dream_idx_type thread_n = 0; thread_n < nthreads; thread_n++) {
 
-    col_start = thread_n * A_N/nthreads;
-    col_stop =  (thread_n+1) * A_N/nthreads;
+    dream_idx_type col_start = thread_n * A_N/nthreads;
+    dream_idx_type col_stop =  (thread_n+1) * A_N/nthreads;
 
     // Init local data.
     D[thread_n].col_start = col_start; // Local start index;
@@ -369,7 +245,7 @@ Copyright @copyright{} 2006-2023 Fredrik Lingvall.\n\
 
   if (nthreads > 1) {
     // Wait for all threads to finish.
-    for (octave_idx_type thread_n = 0; thread_n < nthreads; thread_n++) {
+    for (dream_idx_type thread_n = 0; thread_n < nthreads; thread_n++) {
       threads[thread_n].join();
     }
   }
@@ -395,10 +271,16 @@ Copyright @copyright{} 2006-2023 Fredrik Lingvall.\n\
     std::cerr << "Couldn't register old SIGINT signal handler!" << std::endl;
   }
 
-  if (!running) {
-    dream_err_msg("CTRL-C pressed!\n"); // Bail out.
-    return oct_retval;
-  }
+  return jl_Y_mat;
+}
 
-  return oct_retval;
+/***
+ *
+ *  Julia gateway function for (parallel) dreamrect.
+ *
+ ***/
+
+JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
+{
+  mod.method("conv_p", jl_conv_p);
 }
